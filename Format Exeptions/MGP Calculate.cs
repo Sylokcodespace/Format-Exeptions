39a63ea61eefffe3e4c4b449fe2c63ccd63c87d3
brady <brady@DESKTOP-F5QNSUB>
2025-01-30 04:26:17 +0900

    Add project files.

---
+++ Format Exeptions/MGP Calculate.cs
@@ -0,0 +1,50 @@
+﻿namespace Format_Exeptions
+{
+    public class MGP_Calculate
+    {
+        private string? miles;
+        private string? gallons;
+
+        double CalculateMPG(double miles, double gallons)
+        {
+            if (gallons == 0)
+            {
+                throw new DivideByZeroException("gallons used ccannot be zero.");
+            }
+            return miles / gallons;
+        }
+
+        public void GetUsterInput()
+        {
+            bool validInput = false;
+
+            while (!validInput) // loop until valid input is entered
+            {
+                try
+                {
+                    Console.WriteLine("Enter miles driven: ");
+                    miles = Console.ReadLine();
+                    Console.WriteLine("Enter gallons used: ");
+                    gallons = Console.ReadLine();
+
+                    double milesDriven = Convert.ToDouble(miles);
+                    double gallonsUsed = Convert.ToDouble(gallons);
+
+
+                    double mgp = CalculateMPG(milesDriven, gallonsUsed);
+                    Console.WriteLine($"Miles per gallon: {mgp}");
+
+                    validInput = true; // if no exception, input is valid, exit the loop and allow user to exit the program
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input! please enter a valid number.");
+                }
+                catch (DivideByZeroException dbze)
+                {
+                    Console.WriteLine($"{dbze.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ Format Exeptions/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Format_Exeptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // create instance of MGP_calculate
+            MGP_Calculate mGP_Calculate = new MGP_Calculate();
+
+            // Call getuser input method to start the application
+            mGP_Calculate.GetUsterInput();
+        }
+    }
+}
